4c8b0bc9db2becd662c7b553dcb1bb5bb5baa314
victor <guerinonvictor@outlook.fr>
2021-12-04 21:59:30 +0100

    Header + burger menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./home-page/home-page.component";
+import {TemplateGeneriqueComponent} from "./template-generique/template-generique.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TemplateGeneriqueComponent,
+    children: [
+      {
+        path: '',
+        component: HomePageComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/template-generique/template-generique.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TemplateGeneriqueComponent } from './template-generique.component';
+
+describe('TemplateGeneriqueComponent', () => {
+  let component: TemplateGeneriqueComponent;
+  let fixture: ComponentFixture<TemplateGeneriqueComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TemplateGeneriqueComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TemplateGeneriqueComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
