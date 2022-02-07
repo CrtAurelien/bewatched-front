0ef133698299ea263d496bbc18bc1bcb75f90144
victor <guerinonvictor@outlook.fr>
2022-02-07 18:24:54 +0100

    component

---
+++ src/app/shop/recap-commande/recap-commande.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecapCommandeComponent } from './recap-commande.component';
+
+describe('RecapCommandeComponent', () => {
+  let component: RecapCommandeComponent;
+  let fixture: ComponentFixture<RecapCommandeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RecapCommandeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RecapCommandeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shop/recap-commande/recap-commande.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recap-commande',
+  templateUrl: './recap-commande.component.html',
+  styleUrls: ['./recap-commande.component.scss']
+})
+export class RecapCommandeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
